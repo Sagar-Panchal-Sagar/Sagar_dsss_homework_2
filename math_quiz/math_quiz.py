b485acde9049afca99b5dd195e77ecec6b05ed35
Sagar-Panchal-Sagar <150721435+Sagar-Panchal-Sagar@users.noreply.github.com>
2023-11-14 00:50:18 +0530

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,18 +1,18 @@
 import random                                # Importing the package random
 
 
-def function_A(min, max):                    # Defining a function called function_A which returns a random integer between 1 and 20
+def Rand_int_generator(min, max):            # Defining a function which returns a random integer between 1 and 20
     """
     Random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():                             # Defining a function called function_A which returns a random choice of operators i.e. '+', '-', '*'
+def Rand_operator_generator():                # Defining a function which returns a random choice of operators i.e. '+', '-', '*'
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):                    # Defining a function called function_A which defines some operations according to the choice of operators i.e. '+', '-', '*'and returns p and a
+def Operation_generator(n1, n2, o):           # Defining a function which defines some operations according to the choice of operators i.e. '+', '-', '*'and returns p and a
     p = f"{n1} {o} {n2}"
     if o == '+': a = n1 - n2
     elif o == '-': a = n1 + n2
@@ -27,11 +27,15 @@ def math_quiz():                              # Defining a function called math_
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = Rand_int_generator(1, 10); n2 = Rand_int_generator(1, 5.5); o = Rand_operator_generator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Operation_generator(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
+        try:
+            if type(useranswer) == int || float
+        except:
+            print("Enter a valid number")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
