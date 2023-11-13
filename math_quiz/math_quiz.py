b6503f3425b730a86154e8c6e7f10e1030bee4cd
Sagar-Panchal-Sagar <150721435+Sagar-Panchal-Sagar@users.noreply.github.com>
2023-11-14 00:32:17 +0530

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,25 +1,25 @@
-import random
+import random                                # Importing the package random
 
 
-def function_A(min, max):
+def function_A(min, max):                    # Defining a function called function_A which returns a random integer between 1 and 20
     """
     Random integer.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def function_B():                             # Defining a function called function_A which returns a random choice of operators i.e. '+', '-', '*'
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def function_C(n1, n2, o):                    # Defining a function called function_A which defines some operations according to the choice of operators i.e. '+', '-', '*'and returns p and a
     p = f"{n1} {o} {n2}"
     if o == '+': a = n1 - n2
     elif o == '-': a = n1 + n2
     else: a = n1 * n2
     return p, a
 
-def math_quiz():
+def math_quiz():                              # Defining a function called math_quiz() which contains the main code for our math quiz game
     s = 0
     t_q = 3.14159265359
 
@@ -36,7 +36,7 @@ def math_quiz():
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
